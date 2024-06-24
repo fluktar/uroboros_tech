bc37230b3449562cb7819a2f01d568c002799f87
fluktar <tyc.pawel@gmail.com>
2024-06-24 11:48:24 +0200

    chore: Remove unnecessary routes and update navigation styling

--- routes/link.js
+++ routes/link.js
@@ -6,28 +6,6 @@ router.get("/", (req, res, next) => {
   res.render("index", { mapKey });
 });
 
-router.get("/zt", (req, res) => {
-  res.render("zt", { mapKey });
-});
-router.get("/za", (req, res) => {
-  res.render("za", { mapKey });
-});
-router.get("/up", (req, res) => {
-  res.render("up", { mapKey });
-});
-router.get("/op", (req, res) => {
-  res.render("op", { mapKey });
-});
-router.get("/rzk", (req, res) => {
-  res.render("rzk", { mapKey });
-});
-router.get("/security", (req, res) => {
-  res.render("security", { mapKey });
-});
-router.get("/cid", (req, res) => {
-  res.render("cid", { mapKey });
-});
-
 router.post("/send-message", (req, res) => {
   const message = req.body.message;
   const email = req.body.email;
